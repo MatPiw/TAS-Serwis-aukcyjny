870748b8d7f7409a86d79b89d2ec0fa44d6e0ecf
MatPiw <mateusz.piwecki@wp.pl>
2015-11-03 13:02:03 +0100

    update

--- JavaAPI/src/main/java/pl/edu/amu/MyApplication.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.amu;
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class MyApplication extends ResourceConfig {
-    public MyApplication () {
-        register(HelloResource.class);
-    }
-}
