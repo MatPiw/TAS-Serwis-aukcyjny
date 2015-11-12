cbbf887d100989c6a68010a9b550a7b95a686784
unknown <s396346@pc15m.labs.wmi.amu.edu.pl>
2015-11-12 18:49:14 +0100

    Strona 1.1

---
+++ serwis/appl_serwis/view/script/db.php
@@ -0,0 +1,4 @@
+<?php
+mysql_connect("http://serwer1464462.home.pl/sql/","14622709_tas","TASY!2015") or die(mysql_error()."Nie mozna polaczyc sie z baza danych.");
+mysql_select_db("14622709_tasy") or die(mysql_error()."Nie mozna wybrac bazy danych.");
+?>
