3ed7e503cbe53bf216a8b9481a4957df4461029c
miszal <mickla@st.amu.edu.pl>
2015-12-25 17:44:25 +0100

    Czystki w skryptach + walidacja edycji profilu

---
+++ serwis/appl_serwis/view/script/validateFields.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Miszal on 2015-12-25.
+ */
+
+    function validateForm()
+    {
+        var number = document.getElementById('phone').value;
+        var len = number.length;
+        if( document.getElementById('fname').value != '' &&
+            document.getElementById('lname').value != '' &&
+            document.getElementById('email').value != '' &&
+            document.getElementById('city').value != '' &&
+            document.getElementById('address').value != '' &&
+            document.getElementById('zipc').value != '' &&
+            isNaN(document.getElementById('phone').value) === false &&
+            len != 12)
+        {
+            document.getElementById("edit").submit();
+        }
+        else
+        {
+            alert('Wprowadzono złe dane');
+        }
+    }
